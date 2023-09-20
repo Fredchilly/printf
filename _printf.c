c5fc902406d9519bf585b4d88ff9a1a6ea68d227
fredchilly <fredchillyonline@gmail.com>
2023-09-20 16:06:37 -0700

    main.h

--- _printf.c
+++ _printf.c
@@ -3,15 +3,14 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - take variadic number of arguments.
- * @format: fix variable in printf variadic function.
- * Return: the printed chars.
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	int p_width, flag, p_precision, p_size;
-	int p_buff_ind = 0;
-	int k, printed = 0, printed_chars = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -20,33 +19,33 @@ int _printf(const char *format, ...)
 
 	va_start(list, format);
 
-	for (k = 0; format && format[k] != '\0'; k++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[k] != '%')
+		if (format[i] != '%')
 		{
-			buffer[p_buff_ind++] = format[k];
-			if (p_buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &p_buff_ind);
-
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
 		{
-			print_buffer(buffer, &p_buff_ind);
-			flag = get_flags(format, &k);
-			p_width = get_width(format, &k, list);
-			p_precision = get_precision(format, &k, list);
-			p_size = get_size(format, &k);
-			++k;
-			printed = handle_print(format, &k, list, buffer,
-				flag, p_width, p_precision, p_size);
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+					       flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
 		}
 	}
 
-	print_buffer(buffer, &p_buff_ind);
+	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
@@ -54,8 +53,8 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the buffer contents
- * @buffer: take array of chars
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
